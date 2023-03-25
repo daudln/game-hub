b26156e546e6fee6a9e449dc0802f0b9fdc42298
DaudLN <daudnamayala@gmail.com>
2023-03-25 10:39:06 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button, ButtonGroup, Text, HStack } from "@chakra-ui/react";
+
+const App = () => (
+  <HStack>
+    <Button colorScheme="blue">Button</Button>
+    <Text>Hello world</Text>
+  </HStack>
+);
+
+export default App;
