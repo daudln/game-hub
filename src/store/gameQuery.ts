1fa5080864fcfe04a2019bd1877f0d8118e7f2be
Daud L Namayala <daudnamayala@gmail.com>
2023-05-18 19:07:07 +0300

    persisting user query options

--- src/store/gameQuery.ts
+++ src/store/gameQuery.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { devtools } from 'zustand/middleware';
+import { devtools, persist } from 'zustand/middleware';
 
 export interface GameQuery {
   genreId?: number;
@@ -16,16 +16,20 @@ interface GameQueryStore {
   setSearchInput: (searchInput: string) => void;
 }
 
-const store = devtools<GameQueryStore>((set) => ({
-  gameQuery: {},
-  setGenreId: (genreId) =>
-    set((store) => ({ gameQuery: { ...store.gameQuery, genreId } })),
-  setSortOrder: (sortOrder) =>
-    set((store) => ({ gameQuery: { ...store.gameQuery, sortOrder } })),
-  setPlatformId: (platformId) =>
-    set((store) => ({ gameQuery: { ...store.gameQuery, platformId } })),
-  setSearchInput: (searchInput) => set(() => ({ gameQuery: { searchInput } })),
-}));
+const store = persist(
+  devtools<GameQueryStore>((set) => ({
+    gameQuery: {},
+    setGenreId: (genreId) =>
+      set((store) => ({ gameQuery: { ...store.gameQuery, genreId } })),
+    setSortOrder: (sortOrder) =>
+      set((store) => ({ gameQuery: { ...store.gameQuery, sortOrder } })),
+    setPlatformId: (platformId) =>
+      set((store) => ({ gameQuery: { ...store.gameQuery, platformId } })),
+    setSearchInput: (searchInput) =>
+      set(() => ({ gameQuery: { searchInput } })),
+  })),
+  { name: 'user-query-options' }
+);
 const useGameQueryStore = create(store);
 
 export default useGameQueryStore;
